d5fc923d4e8897c7b570bff736e4604bc277c00f
Sergey Rybakin <sergey.rybakin@gmail.com>
2023-01-02 11:53:03 +0700

    Answer to Task1_3: Creation of GitHub repository

---
+++ src/test/java/HelloFromTask3Test.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloFromTask3Test {
+    @Test
+    public void testHelloFromTask3 () {
+        System.out.println("Hello from Sergey Rybakin");
+    }
+}
