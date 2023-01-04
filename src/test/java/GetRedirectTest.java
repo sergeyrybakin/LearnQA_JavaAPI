34abf8ff6144d4fff27dff16e44661149cef9ef1
Sergey Rybakin <sergey.rybakin@gmail.com>
2023-01-04 19:37:02 +0700

    Ex6: Redirection

---
+++ src/test/java/GetRedirectTest.java
@@ -0,0 +1,26 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+
+import org.junit.jupiter.api.Test;
+
+public class GetRedirectTest {
+    @Test
+    public void testGetRedirect(){
+        Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)//to stop redirection it should be false
+                    .when()
+                    .get("https://playground.learnqa.ru/api/long_redirect")
+                    .andReturn();
+
+        System.out.println("\nAll Headers are below:");
+        Headers allHeaders = response.getHeaders();
+        System.out.println(allHeaders);
+
+        System.out.println("\n\nRedirection URL is below:");
+        String redirectionURL = response.getHeader("Location");
+        System.out.println(redirectionURL);
+    }
+}
