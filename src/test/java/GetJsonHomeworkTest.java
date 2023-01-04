327c2a1c686c677267a79c781f64a56ecb83c8aa
Sergey Rybakin <sergey.rybakin@gmail.com>
2023-01-04 18:49:08 +0700

    Ex5: JSON parsing hometask

---
+++ src/test/java/GetJsonHomeworkTest.java
@@ -0,0 +1,19 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
+import org.junit.jupiter.api.Test;
+
+public class GetJsonHomeworkTest {
+    @Test
+    public void testGetJsonHomework (){
+        JsonPath jsonPath = RestAssured
+                .get("https://playground.learnqa.ru/api/get_json_homework")
+                .jsonPath();
+        System.out.println("\nWhole JSON text is below:");
+        jsonPath.prettyPrint();
+
+        System.out.println("\nThe second message text is below:");
+        String secondMessage = jsonPath.get("messages[1].message");
+        System.out.println(secondMessage);
+    }
+}
