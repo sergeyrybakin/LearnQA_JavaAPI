907a0eb8c1137fb9895097525ef9e3ab26c3c2ce
Sergey Rybakin <sergey.rybakin@gmail.com>
2023-01-04 12:40:04 +0700

    add FizzBuzzKISSTest

---
+++ src/test/java/FizzBuzzKISSTest.java
@@ -0,0 +1,38 @@
+import io.qameta.allure.Description;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzKISSTest
+{
+    @Test
+    @Tag("FizzBuzz")
+    @Description("Type numbers from 1 to 100."
+            + "The program should output the word \"Fizz\" instead of numbers divisible by 3,"
+            + "and the word \"Buzz\" instead of numbers divisible by 5."
+            + "If the number is a multiple of both 3 and 5, the program should output the word \"FizzBuzz\".")
+    public void fizzBuzzKISStest() {
+        String s;
+        int i = 1;
+        for(;i < 101; i++)
+        {
+            if (i % 3 > 0)
+            { //non divisible on 3
+                if (i % 5 > 0)
+                { //non divisible on 5
+                    s = String.valueOf(i);
+                }
+                else { //divisible on 5
+                    s = "Buzz";
+                }
+            }
+            else
+            {
+                s = "Fizz";
+                if (i % 5 == 0)
+                    s = s.concat("Buzz");
+            }
+            System.out.println(s + "\n");
+        }
+    }
+}
