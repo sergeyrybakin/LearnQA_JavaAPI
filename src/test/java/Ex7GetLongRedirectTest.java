9a22be3d6828eee1585d9c68a4c97254cb6fcea4
Sergey Rybakin <sergey.rybakin@gmail.com>
2023-01-05 13:09:08 +0700

    Ex7: Long redirection

---
+++ src/test/java/Ex7GetLongRedirectTest.java
@@ -0,0 +1,44 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+public class Ex7GetLongRedirectTest
+{
+    @Test
+    public void testEx7GetLongRedirect(){
+        Map<Integer, String> links = new HashMap<>();
+        int i=0;
+        int statusCode;
+        links.put(i,"https://playground.learnqa.ru/api/long_redirect");
+        String redirectionURL;
+
+        do
+        {
+            Response response = RestAssured
+                    .given()
+                    .redirects()
+                    .follow(false)//to stop redirection it should be false
+                    .when()
+                    .get(links.get(i))
+                    .andReturn();
+
+            redirectionURL = response.getHeader("Location");
+            statusCode = response.getStatusCode();
+            i = i+1;
+            if(redirectionURL!=null){
+                links.put(i, redirectionURL);
+            }
+        } while (statusCode!=200);
+        //Output of all links
+        links.forEach((k, v) -> {
+            System.out.println(" " + k + "   Link: " + v);
+        });
+        System.out.println("Total amount of redirections is: " + links.size());
+    }
+}
